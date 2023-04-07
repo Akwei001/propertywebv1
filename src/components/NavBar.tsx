f9c0298d2ad4f0bc0b6a2b354fb830ad5606328d
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-04-07 21:35:28 +0100

    components added

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NavBar = () => {
+  return <div className='text-blue-600 text-8xl'>NavBar</div>;
+};
+
+export default NavBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import NavBar from '@/components/NavBar';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
-      <div>New App</div>
+      <NavBar />
+      <Header />
+      <Footer />
     </>
   );
 }
