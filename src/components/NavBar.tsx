429502d265d8bea3b85a759dca161539c0eb5283
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-04-08 18:03:46 +0100

    basic navbar set up

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const NavBar = () => {
-  return <div className='text-blue-600 text-8xl'>NavBar</div>;
+  return (
+    <>
+      <div className='container flex flex-col flex-wrap p-5 mx-auto'>
+        <a className='flex items-center mb-4 font-medium text-gray-900 title-font'>
+          PropertyWeb
+        </a>
+        <div className='flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto'>
+          <a className='mr-5'>First Link</a>
+          <a className='mr-5'>Second Link</a>
+          <a className='mr-5'>Third Link</a>
+          <a className='mr-5'>Fourth Link</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
