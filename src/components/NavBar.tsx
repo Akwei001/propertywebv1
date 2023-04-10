1f79e0abb25a382fa7d299059df3553773540d75
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-04-10 15:40:52 +0100

    navbar bg color black

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const NavBar = () => {
   return (
     <>
-      <div className='container flex flex-col flex-wrap p-5 mx-auto'>
-        <a className='flex items-center mb-4 font-medium text-gray-900 title-font'>
+      <div className='flex flex-col flex-wrap p-5 mx-auto '>
+        <a className='flex items-center font-medium text-white bg-black title-font'>
           PropertyWeb
         </a>
-        <div className='flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto'>
+        <div className='flex flex-wrap justify-center text-base text-white bg-black md:ml-auto md:mr-auto'>
           <a className='mr-5'>First Link</a>
           <a className='mr-5'>Second Link</a>
           <a className='mr-5'>Third Link</a>
