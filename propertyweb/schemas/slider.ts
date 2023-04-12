f405b1b658009b8b2638a75526a3d76f789a8a15
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-04-12 21:16:06 +0100

    slider ts added

---
+++ propertyweb/schemas/slider.ts
@@ -0,0 +1,35 @@
+export default {
+  name: 'banner',
+  title: 'Banner',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'buttonText',
+      title: 'ButtonText',
+      type: 'string',
+    },
+    {
+      name: 'largeText1',
+      title: 'LargeText1',
+      type: 'string',
+    },
+    {
+      name: 'midText',
+      title: 'MidText',
+      type: 'string',
+    },
+    {
+      name: 'smallText',
+      title: 'SmallText',
+      type: 'string',
+    },
+  ],
+}
