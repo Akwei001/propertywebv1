ecb2c99c301826c026e3e7a11cdb54431385b06a
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-03 21:49:38 +0100

    tailwind working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import NavBar from '@/components/NavBar';
 import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+// import { Footer } from '@/components/Footer';
 
 import styles from '@/styles/Home.module.css';
 import Slider from '@/components/Slider';
@@ -13,7 +13,7 @@ export default function Home() {
       <NavBar />
       <Slider />
       <Header />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
