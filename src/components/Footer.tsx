c3fdd2a6934b0dedba0a1a11c73cdb760a1b2d4b
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-06 17:53:25 +0100

    footer text added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,37 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='flex flex-wrap p-5 text-white bg-black justify-evenly'>
-      <h2>Information</h2>
-      <h2>Our Products</h2>
+    <footer className='flex flex-wrap p-5 text-white bg-black justify-evenly'>
+      <ul>
+        <h2 className='font-bold'>Information</h2>
+        <li>Information for Sellers</li>
+        <li>Information for Buyers</li>
+        <li>Information for Landlords</li>
+        <li>Information for Tenants</li>
+        <li>Information for Developers</li>
+      </ul>
+      <ul>
+        <h2 className='font-bold'>Our Services</h2>
+        <li>Residential Sales</li>
+        <li>Residential lettings</li>
+        <li>New Land</li>
+        <li>Asset Management</li>
+      </ul>
+      <ul>
+        <h2 className='font-bold'>Help & Contact</h2>
+        <li>Contact Us</li>
+        <li>FAQ</li>
+        <li>Support</li>
+      </ul>
+      <ul>
+        <h2 className='font-bold'>Connect</h2>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Instagram</li>
+      </ul>
 
-      <h2>Our Services</h2>
-      <h2>Contact Us</h2>
-      <h2>About Us</h2>
-    </div>
+      <h2></h2>
+    </footer>
   );
 };
 
