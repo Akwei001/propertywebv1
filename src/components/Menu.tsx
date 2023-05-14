ab4ae43fca5eff592e8ab6f03de4784b4c9e5376
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-14 11:52:12 +0100

    menu dropdowns functional

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,9 +3,17 @@ import Link from 'next/link';
 
 export const Menu = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showDropdown1, setShowDropdown1] = useState(false);
+  const [showDropdown2, setShowDropdown2] = useState(false);
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
+  };
+  const toggleDropdown1 = () => {
+    setShowDropdown1(!showDropdown1);
+  };
+  const toggleDropdown2 = () => {
+    setShowDropdown2(!showDropdown2);
   };
   return (
     <>
@@ -20,12 +28,47 @@ export const Menu = () => {
               </div>
               <div className='hidden md:block'>
                 <div className='flex items-baseline ml-10 space-x-4'>
-                  <Link href='/'>
-                    <span className='px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white'>
-                      Home
-                    </span>
-                  </Link>
                   <div className='relative'>
+                    <button
+                      className='px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+                      onClick={toggleDropdown1}
+                    >
+                      Home
+                    </button>
+                    {showDropdown1 && (
+                      <div
+                        className='absolute right-0 z-10 w-56 mt-2 origin-top-left bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5'
+                        role='menu'
+                        aria-orientation='vertical'
+                        aria-labelledby='options-menu'
+                      >
+                        <Link href='/'>
+                          <span
+                            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                            role='menuitem'
+                          >
+                            Lorem ipsum.
+                          </span>
+                        </Link>
+                        <Link href='/'>
+                          <span
+                            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                            role='menuitem'
+                          >
+                            Lorem ipsum.
+                          </span>
+                        </Link>
+                        <Link href='/'>
+                          <span
+                            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                            role='menuitem'
+                          >
+                            Lorem ipsum.
+                          </span>
+                        </Link>
+                      </div>
+                    )}
+
                     <button
                       className='px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
                       onClick={toggleDropdown}
@@ -66,10 +109,46 @@ export const Menu = () => {
                       </div>
                     )}
                   </div>
-                  <Link href='/about'>
-                    <span className='px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white'>
-                      About
-                    </span>
+                  <Link href='/'>
+                    <button
+                      className='px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white'
+                      onClick={toggleDropdown2}
+                    >
+                      Lorem.
+                    </button>
+                    {showDropdown2 && (
+                      <div
+                        className='absolute right-0 z-10 w-56 mt-2 origin-top-left bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5'
+                        role='menu'
+                        aria-orientation='vertical'
+                        aria-labelledby='options-menu'
+                      >
+                        <Link href='/'>
+                          <span
+                            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                            role='menuitem'
+                          >
+                            Lorem ipsum.
+                          </span>
+                        </Link>
+                        <Link href='/'>
+                          <span
+                            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                            role='menuitem'
+                          >
+                            Lorem ipsum.
+                          </span>
+                        </Link>
+                        <Link href='/'>
+                          <span
+                            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                            role='menuitem'
+                          >
+                            Lorem ipsum.
+                          </span>
+                        </Link>
+                      </div>
+                    )}
                   </Link>
                 </div>
               </div>
