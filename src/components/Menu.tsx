dca9695253e3e65834fd6b4c3de2380d14916c5f
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-14 11:58:30 +0100

    menu margin changed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -18,12 +18,12 @@ export const Menu = () => {
   return (
     <>
       <nav className='bg-gray-800'>
-        <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+        <div className='px-4 mx-auto sm:px-6 lg:px-8'>
           <div className='flex items-center justify-end h-16'>
             <div className='flex items-center'>
               <div className='flex-shrink-0'>
                 <Link href='/'>
-                  <span className='text-xl font-bold text-white'>My Site</span>
+                  <span className='text-xl font-bold text-white'></span>
                 </Link>
               </div>
               <div className='hidden md:block'>
