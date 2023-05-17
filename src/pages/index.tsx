723a1d935e8220e186a7b8c11ab81fe737234342
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-17 10:48:52 +0100

    FeatureSection compnent added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import NavBar from '@/components/NavBar';
 import CTA from '@/components/CTA';
 import Footer from '@/components/Footer';
 import { Menu } from '@/components/Menu';
-
+import FeatureSection from '@/components/FeatureSection';
 import Slider from '@/components/Slider';
 
 export default function Home() {
@@ -13,6 +13,7 @@ export default function Home() {
       <NavBar />
       <Menu />
       <Slider />
+      <FeatureSection />
       <CTA />
       <Footer />
     </>
