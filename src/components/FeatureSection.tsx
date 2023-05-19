2890bc8fcc5e1e94a51c9845aaf77bc50de896ba
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-19 10:49:59 +0100

    feature basic layout

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const FeatureSection = () => {
   return (
-    <div>FeatureSection</div>
-  )
-}
+    <>
+      <div className='p-4 m-4 border border-gray-300 b-2'>
+        <h2>Title</h2>
+        <p>Subtitle</p>
+        <div>Image</div>
+      </div>
+      <div className='p-4 m-4 border border-gray-300 b-2'>
+        <h2>Title</h2>
+        <p>Subtitle</p>
+        <div>Image</div>
+      </div>
+    </>
+  );
+};
 
-export default FeatureSection
+export default FeatureSection;
