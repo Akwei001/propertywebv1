5882d201c95f298ffeb3948aa473f61a47967b2e
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-23 11:47:47 +0100

    feature section image moved

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const FeatureSection = () => {
   return (
     <div className='max-w-6xl p-4 m-4 mx-auto bg-blue-400'>
-      <div className='p-4 m-4 border border-gray-300 rounded-lg b-2'>
+      <div className='m-4 border border-gray-300 rounded-lg flex-colp-4 b-2'>
         <h2>Title</h2>
         <p>Subtitle</p>
-        <div>Image</div>
+        <div className='flex justify-end '>Image</div>
       </div>
       <div className='p-4 m-4 border border-gray-300 rounded-lg b-2'>
         <h2>Title</h2>
