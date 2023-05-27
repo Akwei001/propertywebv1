ea1dc3a40356ce5a75dfbe0185f6ea476877cd1b
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-05-27 10:56:26 +0100

    Menu grid UI update

--- src/components/MenuGrid.tsx
+++ src/components/MenuGrid.tsx
@@ -3,24 +3,36 @@ import React from 'react';
 const MenuGrid = () => {
   return (
     <div className=''>
-      <div className='grid max-w-6xl grid-cols-3 gap-4 mx-auto text-center'>
-        <div className='bg-red-500'>
-          <h2>Residential Sales</h2>
+      <div className='grid max-w-6xl grid-cols-3 gap-4 mx-auto text-base text-center '>
+        <div className=' bg-cover  bg-center bg-black h-[250px] rounded-xl '>
+          <h2 className='mt-10 text-2xl font-bold text-teal-500 hover:text-gray-500 '>
+            Residential Sales
+          </h2>
         </div>
-        <div className='bg-red-500'>
-          <h2>Residential Sales</h2>
+        <div className=' bg-cover  bg-center bg-black   h-[250px] rounded-xl'>
+          <h2 className='mt-10 text-2xl font-bold text-teal-500 hover:text-gray-500'>
+            Residential Lettings
+          </h2>
         </div>
-        <div className='bg-red-500'>
-          <h2>Residential Sales</h2>
+        <div className=' bg-cover  bg-center bg-black h-[250px] rounded-xl'>
+          <h2 className='mt-10 text-2xl font-bold text-teal-500 hover:text-gray-500'>
+            Asset Management
+          </h2>
         </div>
-        <div className='bg-red-500'>
-          <h2>Residential Sales</h2>
+        <div className=' bg-cover  bg-center bg-black  h-[250px] rounded-xl'>
+          <h2 className='mt-10 text-2xl font-bold text-teal-500 hover:text-gray-500 '>
+            New Land
+          </h2>
         </div>
-        <div className='bg-red-500'>
-          <h2>Residential Sales</h2>
+        <div className=' bg-cover  bg-center bg-black  h-[250px] rounded-xl'>
+          <h2 className='mt-10 text-2xl font-bold text-teal-500 hover:text-gray-500 '>
+            Residential Sales
+          </h2>
         </div>
-        <div className='bg-red-500'>
-          <h2>Residential Sales</h2>
+        <div className=' bg-cover  bg-center bg-black  h-[250px] rounded-xl'>
+          <h2 className='mt-10 text-2xl font-bold text-teal-500 hover:text-gray-500 '>
+            Residential Sales
+          </h2>
         </div>
       </div>
     </div>
