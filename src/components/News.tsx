5d1bf9bad5e04c1c1fb98df8ce3a3b95e74bb862
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-06-02 16:40:33 +0100

    news component title added

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -4,6 +4,9 @@ const News = () => {
   return (
     <>
       <div className=''>
+        <h2 className='mb-4 text-4xl font-bold text-center text-teal-500'>
+          News & Updates
+        </h2>
         <div className='grid max-w-6xl grid-cols-3 gap-4 mx-auto mb-6 text-base text-center '>
           <div className=' bg-cover  bg-center bg-black h-[250px] rounded-xl '>
             <h2 className='mt-10 text-2xl font-bold text-teal-500 hover:text-gray-500 '>
