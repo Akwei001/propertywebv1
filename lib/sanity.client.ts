48e640f430ce4da027cf5172274f2a7b0f0bf572
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-07-24 18:36:11 +0100

    tailwind sttyles issue

---
+++ lib/sanity.client.ts
@@ -0,0 +1,16 @@
+import { createClient, type ClientConfig } from '@sanity/client';
+
+// const config: ClientConfig = {
+//   projectId: 'your-project-id',
+//   dataset: 'your-dataset-name',
+//   useCdn: true, // set to `false` to bypass the edge cache
+//   apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+// };
+
+// const client = createClient(config);
+
+// const data = await client.fetch<number>(`count(*)`);
+// // data is typed as `number`
+// console.log(`Number of documents: ${data}`);
+
+// export default client;
